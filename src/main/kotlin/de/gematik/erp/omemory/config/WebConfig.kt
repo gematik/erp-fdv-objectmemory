ffd9d62a944269b03aee3fd47aa5fef4ab88059f
a.ibrokhimov <a.ibrokhimov@gematik.de>
2025-06-24 18:13:17 +0200

    feat: add CORS.txt

--- src/main/kotlin/de/gematik/erp/omemory/config/WebConfig.kt
+++ src/main/kotlin/de/gematik/erp/omemory/config/WebConfig.kt
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 open class WebConfig : WebMvcConfigurer {
+    //This config is necessary to allow CORS in Sping Boot, i.e. allow requests to
+    // Spring Boot service from different origins(e.g. localhost:5173...)
+    // Spring Boot blocks CORS requests per default
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:5173", "https://storage.googleapis.com") // Frontend port
