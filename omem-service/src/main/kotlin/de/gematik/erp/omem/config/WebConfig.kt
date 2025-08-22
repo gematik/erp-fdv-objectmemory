7d033bb41a70010208b81a6971b407019e8f43a5
a.ibrokhimov <a.ibrokhimov@gematik.de>
2025-08-22 15:32:26 +0200

    refactor: create a separate module for the service

---
+++ omem-service/src/main/kotlin/de/gematik/erp/omem/config/WebConfig.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 gematik GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * *******
+ *
+ * For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
+ */
+
+package de.gematik.erp.omem.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class WebConfig : WebMvcConfigurer {
+    //This config is necessary to allow CORS in Sping Boot, i.e. allow requests to
+    // Spring Boot service from different origins(e.g. localhost:5173...)
+    // Spring Boot blocks CORS requests per default
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173", "https://storage.googleapis.com") // Frontend port
+            .allowedMethods("GET", "POST", "PUT", "OPTIONS")
+            .allowedHeaders("*")
+    }
+}
